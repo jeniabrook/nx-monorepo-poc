4a8fec596ecc8e7cab7fb595d40115165343c2d1
Jenia Brook <jenia@themis-tech.io>
2022-11-27 13:37:57 +0200

    Add UI and use it shop app

--- apps/shop/next.config.js
+++ apps/shop/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const withTM = require("next-transpile-modules")(["ui"]);
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withTM(nextConfig);
---
+++ packages/ui/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  onClick?: () => void;
+}
+
+function Button({ children, onClick }: ButtonProps) {
+  return <button onClick={onClick}>{children}</button>;
+}
+
+export default Button;
